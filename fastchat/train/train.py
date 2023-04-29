d471fe8e688c52a8f3ac15dda2d6d74a7baccf9b
Joe Ksiazek <joe@helmux.com>
2023-04-28 23:17:30 -0400

    data format

--- fastchat/train/train.py
+++ fastchat/train/train.py
@@ -77,75 +77,76 @@ def preprocess(
     sources,
     tokenizer: transformers.PreTrainedTokenizer,
 ) -> Dict:
-    conv = get_default_conv_template("vicuna").copy()
-    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+    # conv = get_default_conv_template("vicuna").copy()
+    # roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
 
     # Apply prompt templates
-    conversations = []
-    for i, source in enumerate(sources):
-        if roles[source[0]["from"]] != conv.roles[0]:
-            # Skip the first one if it is not from human
-            source = source[1:]
+    # conversations = []
+    # for i, source in enumerate(sources):
+    #     if roles[source[0]["from"]] != conv.roles[0]:
+    #         # Skip the first one if it is not from human
+    #         source = source[1:]
 
-        conv.messages = []
-        for j, sentence in enumerate(source):
-            role = roles[sentence["from"]]
-            assert role == conv.roles[j % 2], f"{i}"
-            conv.append_message(role, sentence["value"])
-        conversations.append(conv.get_prompt())
+    #     conv.messages = []
+    #     for j, sentence in enumerate(source):
+    #         role = roles[sentence["from"]]
+    #         assert role == conv.roles[j % 2], f"{i}"
+    #         conv.append_message(role, sentence["value"])
+    #     conversations.append(conv.get_prompt())
 
     # Tokenize conversations
     input_ids = tokenizer(
-        conversations,
+        # conversations,
+        sources,
         return_tensors="pt",
         padding="max_length",
         max_length=tokenizer.model_max_length,
         truncation=True,
     ).input_ids
-    targets = input_ids.clone()
+    # targets = input_ids.clone()
 
-    assert conv.sep_style == SeparatorStyle.TWO
+    # assert conv.sep_style == SeparatorStyle.TWO
 
     # Mask targets
-    sep = conv.sep + conv.roles[1] + ": "
-    for conversation, target in zip(conversations, targets):
-        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+    # sep = conv.sep + conv.roles[1] + ": "
+    # for conversation, target in zip(conversations, targets):
+    #     total_len = int(target.ne(tokenizer.pad_token_id).sum())
 
-        rounds = conversation.split(conv.sep2)
-        cur_len = 1
-        target[:cur_len] = IGNORE_TOKEN_ID
-        for i, rou in enumerate(rounds):
-            if rou == "":
-                break
+    #     rounds = conversation.split(conv.sep2)
+    #     cur_len = 1
+    #     target[:cur_len] = IGNORE_TOKEN_ID
+    #     for i, rou in enumerate(rounds):
+    #         if rou == "":
+    #             break
 
-            parts = rou.split(sep)
-            if len(parts) != 2:
-                break
-            parts[0] += sep
-            round_len = len(tokenizer(rou).input_ids)
-            instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+    #         parts = rou.split(sep)
+    #         if len(parts) != 2:
+    #             break
+    #         parts[0] += sep
+    #         round_len = len(tokenizer(rou).input_ids)
+    #         instruction_len = len(tokenizer(parts[0]).input_ids) - 2
 
-            target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
+    #         target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
 
-            cur_len += round_len
-        target[cur_len:] = IGNORE_TOKEN_ID
+    #         cur_len += round_len
+    #     target[cur_len:] = IGNORE_TOKEN_ID
 
-        if False:
-            z = target.clone()
-            z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
-            rank0_print(tokenizer.decode(z))
+    #     if False:
+    #         z = target.clone()
+    #         z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
+    #         rank0_print(tokenizer.decode(z))
 
-        if cur_len < tokenizer.model_max_length:
-            if cur_len != total_len:
-                target[:] = IGNORE_TOKEN_ID
-                rank0_print(
-                    f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
-                    f" (ignored)"
-                )
+    #     if cur_len < tokenizer.model_max_length:
+    #         if cur_len != total_len:
+    #             target[:] = IGNORE_TOKEN_ID
+    #             rank0_print(
+    #                 f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+    #                 f" (ignored)"
+    #             )
 
     return dict(
         input_ids=input_ids,
-        labels=targets,
+        # labels=targets,
         attention_mask=input_ids.ne(tokenizer.pad_token_id),
     )
 
@@ -157,11 +158,11 @@ class SupervisedDataset(Dataset):
         super(SupervisedDataset, self).__init__()
 
         rank0_print("Formatting inputs...")
-        sources = [example["conversations"] for example in raw_data]
+        sources = [example for example in raw_data]
         data_dict = preprocess(sources, tokenizer)
 
         self.input_ids = data_dict["input_ids"]
-        self.labels = data_dict["labels"]
+        # self.labels = data_dict["labels"]
         self.attention_mask = data_dict["attention_mask"]
 
     def __len__(self):
@@ -170,7 +171,7 @@ class SupervisedDataset(Dataset):
     def __getitem__(self, i) -> Dict[str, torch.Tensor]:
         return dict(
             input_ids=self.input_ids[i],
-            labels=self.labels[i],
+            # labels=self.labels[i],
             attention_mask=self.attention_mask[i],
         )
 
